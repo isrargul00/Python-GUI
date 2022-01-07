88009d940859c7d7d80001e61c12922af35575b1
isrargul <isrargul00@gmail.com>
2022-01-07 11:41:58 +0500

    done

---
+++ __init__.py
@@ -0,0 +1,25 @@
+#main.py
+
+from flask import Flask  
+from flask import render_template
+from flaskwebgui import FlaskUI # import FlaskUI
+
+app = Flask(__name__)
+ui = FlaskUI(app, width=500, height=500) # add app and parameters
+
+
+@app.route("/")
+def hello():
+    return " welcome here "  
+    return render_template('index.html')
+
+@app.route("/home", methods=['GET'])
+def home():
+    return "welcome to home" 
+    return render_template('some_page.html')
+
+
+if __name__ == "__main__":
+    # app.run() for debug
+    ui.run()
+   
